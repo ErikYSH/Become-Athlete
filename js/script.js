e2c4ec7616e05527f640430d2347662b2ba2ff28
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2021-11-28 20:36:45 -0800

    modify moving progression bar

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// let i = 0;
+// function move(){
+//     if (i === 0){
+//         i = 1;
+//         let elem = document.getElementById("mybar");
+//         let width = 1;
+//         let id = setInterval(frame, 20);
+//         function frame() {
+//             if (width >= 100){
+//                 clearInterval(id);
+//                 i = 0
+//             }else {
+//                 width ++;
+//                 elem.style.width = width + "%"
+//             }
+//         }
+//     }
+// }
