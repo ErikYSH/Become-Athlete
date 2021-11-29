5c6a1fca12747380129e00d561821893d295d225
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2021-11-29 13:37:32 -0800

    advance bar working

--- js/script.js
+++ js/script.js
@@ -1,22 +1,51 @@
-// let i = 0;
-// function move(){
-//     if (i === 0){
-//         i = 1;
-//         let elem = document.getElementById("mybar");
-//         let width = 1;
-//         let id = setInterval(frame, 20);
-//         function frame() {
-//             if (width >= 100){
-//                 clearInterval(id);
-//                 i = 0
-//             }else {
-//                 width ++;
-//                 elem.style.width = width + "%"
-//             }
-//         }
-//     }
-// }
 
 
 
 
+
+
+
+
+
+
+//** Bulking Bar  */
+
+function bulking() {
+  let bulkEle = document.getElementById("moreCalorie");
+  let width = 100;
+  let time = 1;
+  let timeId = setInterval(frame, 300);
+  function frame() {
+    if (width >= 0) {
+      clearInterval(timeId);
+    } else {
+      width = width - time;
+      bulkEle.style.width = width + "%";
+    }
+  }
+}
+
+bulking();
+
+//** Progression Bar */
+
+i = 0;
+function advance() {
+  if (i === 0) {
+    i = 1;
+    let advanceEle = document.getElementById("advance-bar");
+    let width = 1;
+    let time = setInterval(frame, 200);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(time);
+        i = 0;
+      } else {
+        width++;
+        advanceEle.style.width = width + "%";
+      }
+    }
+  }
+}
+
+advance();
