003d2fe26cd2121f5535a10ab50901c992d3b64c
ErikYSH <Erikyang@Eriks-MacBook-Pro-2.local>
2021-11-29 20:16:57 -0800

    Lost Game section

--- js/script.js
+++ js/script.js
@@ -1,132 +1,129 @@
-
-
-
 // ---- Global Variables ---- //
 
 athlete = {
-    bulking: 100,
-    preWorkout: 100,
-    getFit : 100,
-    advanceBar: 0,
+  bulking: 100,
+  preWorkout: 100,
+  getFit: 100,
+  advanceBar: 0,
 };
 
+let $moreCalorie = $("#moreCalorie");
+let $energyLvl = $("#energyLvl");
+let $workoutSession = $("#workoutSession");
+let $advanceBar = $("#advance-bar");
+let $gamePage = $("game-page");
+let $lostPage = $("#lost-page");
 
-let $moreCalorie = $('#moreCalorie')
-let $energyLvl = $('#energyLvl')
-let $workoutSession = $('#workoutSession')
-let $advanceBar = $('#advance-bar')
+// ---- Losing Game ---- //
 
-
-
-
+function lostGame() {
+  if (
+    athlete.bulking === 0 ||
+    athlete.preWorkout === 0 ||
+    athlete.getFit === 0
+  ) {
+    // $gamePage.fadeOut();
+    // $gamePage.css('display','none');
+    $lostPage.css("display", "block");
+  }
+}
 
 // ---- Decresing Value Bars ---- //
 
-function CalorieLvl (bars, speed){
-bulkingIntervalId =  setInterval(function(){
+function CalorieLvl(bars, speed) {
+  bulkingIntervalId = setInterval(function () {
     athlete.bulking--;
-    bars.css('width',athlete.bulking +"%");
-    bars.html(athlete.bulking + "%")
-    if (athlete.bulking <= 0){
-        clearInterval(bulkingIntervalId)
+    bars.css("width", athlete.bulking + "%");
+    bars.html(athlete.bulking + "%");
+    if (athlete.bulking <= 0) {
+      clearInterval(bulkingIntervalId);
     }
-},speed);
+  }, speed);
 }
 CalorieLvl($moreCalorie, 150);
 
-
-function EnergyLvl(bars, speed){
-    preWorkoutIntervalId = setInterval(function(){
-        athlete.preWorkout--;
-        bars.css('width', athlete.preWorkout + "%");
-        bars.html(athlete.preWorkout + "%");
-        if (athlete.preWorkout <= 0){
-            clearInterval(preWorkoutIntervalId)
-        }
-    },speed)
+function EnergyLvl(bars, speed) {
+  preWorkoutIntervalId = setInterval(function () {
+    athlete.preWorkout--;
+    bars.css("width", athlete.preWorkout + "%");
+    bars.html(athlete.preWorkout + "%");
+    if (athlete.preWorkout <= 0) {
+      clearInterval(preWorkoutIntervalId);
+    }
+  }, speed);
 }
-EnergyLvl($energyLvl,100)
+EnergyLvl($energyLvl, 100);
 
-function WorkoutLvl (bars,speed){
-    getFitIntervalId = setInterval(function(){
-        athlete.getFit--;
-        bars.css('width', athlete.getFit + "%");
-        bars.html(athlete.getFit + "%")
-        if (athlete.getFit<= 0){
-            clearInterval(getFitIntervalId)
-        }
-    }, speed)
+function WorkoutLvl(bars, speed) {
+  getFitIntervalId = setInterval(function () {
+    athlete.getFit--;
+    bars.css("width", athlete.getFit + "%");
+    bars.html(athlete.getFit + "%");
+    if (athlete.getFit <= 0) {
+      clearInterval(getFitIntervalId);
+      lostGame();
+    }
+  }, speed);
 }
-WorkoutLvl($workoutSession, 80)
+WorkoutLvl($workoutSession, 80);
 
+function AdvanceLvl(bars, speed) {
+  advanceBarIntervalId = setInterval(function () {
+    athlete.advanceBar++;
+    bars.css("width", athlete.advanceBar + "%");
+    if (athlete.advanceBar >= 100) {
+      clearInterval(advanceBarIntervalId);
+    } else if (athlete.advanceBar === 50) {
+      bars.html("ADVANCE");
+      console.log("passed 50");
+    } else if (athlete.advanceBar === 75) {
+      bars.html("WORLD CLASS");
+      console.log("pass 75");
+    }
+  }, speed);
+}
 
-function AdvanceLvl (bars,speed){
-    advanceBarIntervalId = setInterval(function (){
-        athlete.advanceBar++;
-        bars.css('width', athlete.advanceBar + "%");
-        if (athlete.advanceBar>= 100){
-            clearInterval(advanceBarIntervalId)
-        } else if (athlete.advanceBar === 50){
-            bars.html('ADVANCE');
-            console.log('passed 50');
-        } else if (athlete.advanceBar === 75){
-            bars.html('WORLD CLASS');
-            console.log("pass 75")
-        }
-    },speed);
-    
-};
-
-
-
-AdvanceLvl($advanceBar, 200); // 500 
-
-
-
+AdvanceLvl($advanceBar, 500);
 
 // ---- Adding Value on Btn Click ---- //
 
-function AddMoreCalLvlValue (add, amount){
-    athlete.bulking = athlete.bulking + amount;
-    add.css('width', athlete.bulking);
-    if(athlete.bulking >= 100){
-        athlete.bulking = 100;
-    }
+function AddMoreCalLvlValue(add, amount) {
+  athlete.bulking = athlete.bulking + amount;
+  add.css("width", athlete.bulking);
+  if (athlete.bulking >= 100) {
+    athlete.bulking = 100;
+  }
 }
 
-function AddEnergyLvlValue (add, amount){
-    athlete.preWorkout = athlete.preWorkout + amount;
-    add.css('width', athlete.preWorkout);
-    if(athlete.preWorkout >= 100){
-        athlete.preWorkout = 100;
-    }
+function AddEnergyLvlValue(add, amount) {
+  athlete.preWorkout = athlete.preWorkout + amount;
+  add.css("width", athlete.preWorkout);
+  if (athlete.preWorkout >= 100) {
+    athlete.preWorkout = 100;
+  }
 }
 
-function AddWorkoutLvlValue (add, amount){
-    athlete.getFit = athlete.getFit + amount;
-    add.css('width', athlete.getFit);
-    if(athlete.getFit >= 100){
-        athlete.getFit = 100;
-    }
+function AddWorkoutLvlValue(add, amount) {
+  athlete.getFit = athlete.getFit + amount;
+  add.css("width", athlete.getFit);
+  if (athlete.getFit >= 100) {
+    athlete.getFit = 100;
+  }
 }
-
-
 
 // ---- Clicked Buttons ---- //
 
 $("#bulking").on("click", function () {
   console.log("Clicked Bulking Btn");
-  AddMoreCalLvlValue($moreCalorie, 5)
+  AddMoreCalLvlValue($moreCalorie, 5);
 });
 
 $("#pre-workout").on("click", function () {
   console.log("Clicked Pre-workout Btn");
-  AddEnergyLvlValue($energyLvl,10);
+  AddEnergyLvlValue($energyLvl, 10);
 });
 
 $("#get-fit").on("click", function () {
   console.log("Clicked Get-fit Btn");
-  AddWorkoutLvlValue($workoutSession,13)
+  AddWorkoutLvlValue($workoutSession, 13);
 });
-
-
